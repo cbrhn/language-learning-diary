4ebab9ef11089016a870f247718f4588714fd656
Weintraube21 <44685102+cbrhn@users.noreply.github.com>
2026-03-06 16:13:45 +0100

    feat: basic initialization and initial db schema

---
+++ backend/langdiary/src/main/java/app/langdiary/LangdiaryApplication.java
@@ -0,0 +1,13 @@
+package app.langdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangdiaryApplication.class, args);
+    }
+
+}
